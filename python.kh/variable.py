d0126ff14aa99809b1478ca3dbf0a2d9d414b3d2
joyenjoy3 <joyenjoy3@naver.com>
2022-11-06 11:21:02 +0900

    2022.11.06 python_kh_2

---
+++ python.kh/variable.py
@@ -0,0 +1,29 @@
+# 이름, 핸드폰 번호, 거주지 : ex) 서울시 강남구...
+
+name = "장정주"
+phone_number = "010-1111-2222"
+address = "서울시 광진구"
+
+print(name)
+print(phone_number)
+print(address)
+
+# 올바른 예
+
+apple = "사과"
+apples = "사과들"
+apples3 = "사과3개"
+app3le = "사3과"
+red_apple = "빨간 사과"
+very_delicious_redapple = "아주 맛있는 빨간 사과"
+
+# 잘못된 예
+
+3apple = "3개의 사과"
+red apple = "빨간 사과"
+apple! = "사과!"
+apple$ = "사과$"
+apple& = "사과&"
+continue =
+if
+for
