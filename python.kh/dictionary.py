7e5f88655f615688a4e6d0625df0e7bd05bbe07d
joyenjoy3 <joyenjoy3@naver.com>
2022-11-06 21:32:13 +0900

    2022.11.06 python_kh_8

---
+++ python.kh/dictionary.py
@@ -0,0 +1,24 @@
+people = {
+  "name": "김개똥",
+  "phone": "010-1010-2020"
+}
+
+print(people["name"], people["phone"])
+
+
+books = {"Daniel Pink":"파는것이 인간이다.", "Eric Schidt": "새로운 디지털 시대"}
+
+print(books["Daniel Pink"])
+
+
+coffee = {"Java":2500, "Americano":2500.,"Latte":3000}
+
+coffee["Java"] = 3000
+
+coffee["Moca"] = 3000
+
+print(coffee["Java"])
+
+print(coffee.values())
+
+print(coffee.items())
