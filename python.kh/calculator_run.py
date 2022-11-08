886892900840de026fbf7dd9f1140e3caf0a8f8e
joyenjoy3 <joyenjoy3@naver.com>
2022-11-08 14:19:28 +0900

    2022.11.08 python_kh_18

---
+++ python.kh/calculator_run.py
@@ -0,0 +1,22 @@
+import calcylator as c
+
+#calcylator.add(10, 20)
+#calcylator.sub(20, 10)
+#calcylator.times(10, 10)
+#calcylator.dev(10, 5)
+
+c.add(10, 20)
+c.sub(10, 20)
+c.times(10, 20)
+c.dev(10, 20)
+
+
+from datetime import date
+
+today = date.today()
+
+print(today)
+
+new_date = date(1999, 12, 31)
+
+print(new_date)
