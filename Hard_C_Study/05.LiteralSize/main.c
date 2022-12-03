be87b5086e46b69db60bf34c5f6ada20392f14a3
joyenjoy3 <joyenjoy3@naver.com>
2022-12-03 21:43:58 +0900

    20221203_Hard_C

---
+++ Hard_C_Study/05.LiteralSize/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("liternal int size: %d \n", sizeof(7));
+	printf("liternal double size: %d \n", sizeof(7.14));
+	printf("literal char size: %d \n", sizeof('A'));
+	return 0;
+}
