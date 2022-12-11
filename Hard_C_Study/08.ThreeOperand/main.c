f04cec8e418a352f15a4e6461be42d5b9ec8b10d
joyenjoy3 <joyenjoy3@naver.com>
2022-12-11 23:23:40 +0900

    20221211_HArd_C

---
+++ Hard_C_Study/08.ThreeOperand/main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num, abs;
+	printf("정수 입력: ");
+	scanf("%d", &num);
+
+	abs = num > 0 ? num : num * (-1);
+	printf("절댓값: %d \n", abs);
+	return 0;
+}
---
+++ Hard_C_Study/08.ifElseBasic/main.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	printf("정수 입력: ");
+	scanf("%d", &num);
+
+	if (num < 0)
+		printf("입력 값은 0보다 작다. \n");
+	else
+		printf("입력 값은 0보다 작지 않다. \n");
+
+	return 0;
+}
