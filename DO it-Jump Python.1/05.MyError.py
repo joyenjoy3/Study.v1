4f875b4cdabb8cee05c10b5cb33b077d63b7e3a8
joyenjoy3 <joyenjoy3@naver.com>
2022-12-18 12:49:33 +0900

    Doit_Python_221218

---
+++ DO it-Jump Python.1/05.MyError.py
@@ -0,0 +1,15 @@
+class MyError(Exception):
+    pass
+
+def say_nick(nick):
+    if nick == '바보':
+        raise MyError()
+    print(nick)
+
+try:
+    say_nick("천사")
+    say_nick("바보")
+except MyError:
+    print("허용되지 않는 별명입니다.")
+
+
